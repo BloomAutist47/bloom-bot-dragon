6a3e32b4a80decdfeebb7e0f55c7448ee6d32e18
BloomAutist47 <BloomAutist47@users.noreply.github.com>
2022-03-02 21:42:29 +0800

    added text embed upload function

--- src/cogs/text.ts
+++ src/cogs/text.ts
@@ -1,4 +1,4 @@
-import { Client, MessageAttachment } from "discord.js"
+import { Client, MessageAttachment, MessageEmbed } from "discord.js"
 import BaseCog from './base'
 import fs from 'fs'
 
@@ -19,8 +19,123 @@ export default class TextCog {
             description: 'Send txt to upload',
         }, false, true)
 
+        this.base.registerCommand(this.cmdUploadText.bind(this), {
+            name: 'uptext',
+            description: 'Upload text list embed',
+        }, false, true)
+
+
     }
 
+    async cmdUploadText(mode: string, source, defered: boolean = false) {
+
+        // Chekc if Bloom
+        if (source.author.id != this.files["resources"]["bloom-id"]) {
+            await source.reply("You're not bloom.")
+            return
+        }
+
+        // Arg check
+        const args = this.base.ContentClean(source.content, 'uptext')
+        if (args == "") {
+            await source.reply("Type a text embed name")
+            return
+        }
+        if (!this.base.files["texts"]["Texts"].hasOwnProperty(args)) {
+            await source.reply("Name does not exists")
+            return
+        }
+        
+        // Clear Channel First
+        await this.clearChat(source)
+
+        // Shortening directory
+        const embedData = this.base.files["texts"]["Texts"][args]
+
+        var links = {}
+        var count = 1
+
+
+
+        // Sending Each Embed Content
+        for (const cont in embedData["content"]) {
+            let title = cont
+            if (args != "botfix") {
+                title = `${count}. ${cont}`
+            }
+            const embed = new MessageEmbed()
+                .setColor(this.base.color)
+                .setTitle(title)
+
+            if (embedData["content"][cont].hasOwnProperty("text")) {
+                embed.setDescription(embedData["content"][cont]["text"])
+            }
+
+            if (embedData["content"][cont].hasOwnProperty("image")) {
+                embed.setImage(embedData["content"][cont]["image"])
+            }
+
+            const item = await source.channel.send({ embeds: [embed] , content: "\u200b"})
+            links[title] = `https://discordapp.com/channels/${item.guildId}/${item.channelId}/${item.id}`
+            count += 1
+        }
+
+        // Creating Table of Content
+        const tableEmbed = new MessageEmbed()
+            .setColor(this.base.color)
+            .setTitle(embedData["title"])
+            .setDescription(embedData["description"])
+        
+        // Botfix special treatment
+        if (args == "botfix") {
+            const botfixLinks: Object = {"Grim":'', "RBot":"", "For Both":""}
+            for (const link in links) {
+                if (link.includes("[Grim]")) {
+                    const title = link.replace("[Grim]", "")
+                    botfixLinks["Grim"] += `[${title}](${links[link]})\n`
+                    continue
+                }
+                if (link.includes("[RBot]")) {
+                    const title = link.replace("[RBot]", "")
+                    botfixLinks["RBot"] += `[${title}](${links[link]})\n`
+                    continue
+                }
+                botfixLinks["For Both"] += `[${link}](${links[link]})\n`
+            }
+
+            tableEmbed.addField("For Both", botfixLinks["For Both"])
+            tableEmbed.addField("Grim", botfixLinks["Grim"])
+            tableEmbed.addField("RBot", botfixLinks["RBot"])
+
+            await source.channel.send({ embeds: [tableEmbed] , content: "\u200b"})
+            return
+        }
+
+        // Appending Links to Table of Content
+        let words = ""
+        let firstField: boolean = false
+        for (const link in links) {
+            if (words.length >= 900) {
+                if (!firstField) {
+                    tableEmbed.addField("Table of Content", words)
+                    words = ""
+                    firstField = true
+                } else {
+                    tableEmbed.addField( "\u200b", words)
+                    words = ""
+                }
+
+            }
+            words += `[${link}](${links[link]})\n`
+            
+        }
+        if (words) {
+            tableEmbed.addField( "\u200b", words)
+        }
+
+        // Send to Channel
+        await source.channel.send({ embeds: [tableEmbed] , content: "\u200b"})
+    }
 
     async cmdUploadFile(mode: string, source, defered: boolean = false) {
 
@@ -75,4 +190,13 @@ export default class TextCog {
 
         
     }
+
+    async clearChat (source) {
+        const channel = source.channel;
+        const messageManager = channel.messages;
+        const messages = await messageManager.channel.messages.fetch({ limit: 100 });
+        await channel.bulkDelete(messages,true);
+    
+    }
+
 }
